1645e84fbb1574fc7c37c1b68489498200748bdb
MD. Assaduzzaman Ashique <ashique12301017@gmail.com>
2019-09-23 15:10:25 +1000

    Add files via upload
    Need to download Standard Asset Package from Unity Asset Store to see the terrain.

---
+++ Assets/Scripts/Flying.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flying : MonoBehaviour {
+
+    public float speed = 2.0f;
+	// Use this for initialization
+	void Start () {
+		this.gameObject.GetComponent <Transform> ().position = new Vector3 (0, 0, -5);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.gameObject.GetComponent <Transform> ().position = this.gameObject.GetComponent <Transform> ().position + speed * this.gameObject.GetComponent <Transform> ().forward;
+
+		float hori = Input.GetAxis ("Horizontal");
+		float vert = Input.GetAxis ("Vertical");
+		this.gameObject.GetComponent <Transform> ().rotation = this.gameObject.GetComponent <Transform> ().rotation * Quaternion.AngleAxis (vert, new Vector3 (-1, 0, 0));
+		this.gameObject.GetComponent <Transform> ().rotation = this.gameObject.GetComponent <Transform> ().rotation * Quaternion.AngleAxis (hori, new Vector3 (0, 1, 0));
+	}
+}
